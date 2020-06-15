b8111ae11c44a53d7c6c8f8e8ba3c568d806f15a
Forrest <dongwenquan@hotmail.co.nz>
2020-06-15 17:05:24 +0800

    GYP-16 [UI component] Rectangle button
    1. update file stucture
    2. update tslint rules

---
+++ projects/gypsophils-ui/src/button/button.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'button[gyp-button]',
+    exportAs: 'gypButton',
+    templateUrl: 'button.html'
+})
+
+export class GypButton implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
