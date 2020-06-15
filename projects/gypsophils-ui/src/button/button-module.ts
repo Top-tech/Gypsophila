a3005ce7a9e0333c3ac01f3e587f87d81fe84836
Forrest <dongwenquan@hotmail.co.nz>
2020-06-15 17:39:23 +0800

    GYP-16 [UI component] Rectangle button
    1. button

---
+++ projects/gypsophils-ui/src/button/button-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { GypButton } from './button';
+
+@NgModule({
+    imports: [],
+    exports: [GypButton],
+    declarations: [GypButton],
+    providers: [],
+})
+export class GypButtonModule {
+}
